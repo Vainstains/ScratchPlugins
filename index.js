96a3c07df8640c0a690b819d97ba6941a087878b
Daniel Aspinwall <84806642+Vainstains@users.noreply.github.com>
2022-07-06 22:45:27 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,110 @@
+
+// create by scratch3-extension generator
+const ArgumentType = Scratch.ArgumentType;
+const BlockType = Scratch.BlockType;
+const formatMessage = Scratch.formatMessage;
+const log = Scratch.log;
+
+const menuIconURI = null;
+const blockIconURI = null;
+
+class testExt{
+  constructor (runtime){
+    this.runtime = runtime;
+    // communication related
+    this.comm = runtime.ioDevices.comm;
+    this.session = null;
+    this.runtime.registerPeripheralExtension('testExt', this);
+    // session callbacks
+    this.reporter = null;
+    this.onmessage = this.onmessage.bind(this);
+    this.onclose = this.onclose.bind(this);
+    this.write = this.write.bind(this);
+    // string op
+    this.decoder = new TextDecoder();
+    this.lineBuffer = '';
+  }
+
+  onclose (){
+    this.session = null;
+  }
+
+  write (data, parser = null){
+    if (this.session){
+      return new Promise(resolve => {
+        if (parser){
+          this.reporter = {
+            parser,
+            resolve
+          }
+        }
+        this.session.write(data);
+      })
+    }
+  }
+
+  onmessage (data){
+    const dataStr = this.decoder.decode(data);
+    this.lineBuffer += dataStr;
+    if (this.lineBuffer.indexOf('\n') !== -1){
+      const lines = this.lineBuffer.split('\n');
+      this.lineBuffer = lines.pop();
+      for (const l of lines){
+        if (this.reporter){
+          const {parser, resolve} = this.reporter;
+          resolve(parser(l));
+        };
+      }
+    }
+  }
+
+  scan (){
+    this.comm.getDeviceList().then(result => {
+        this.runtime.emit(this.runtime.constructor.PERIPHERAL_LIST_UPDATE, result);
+    });
+  }
+
+  getInfo (){
+    return {
+      id: 'testExt',
+      name: 'Test',
+      color1: '#0FBD8C',
+      color2: '#0DA57A',
+      menuIconURI: menuIconURI,
+      blockIconURI: blockIconURI,
+      blocks: [
+        {
+          opcode: 'Distance',
+          blockType: BlockType.REPORTER,
+          arguments: {
+            X1: {
+              type: ArgumentType.STRING
+            },
+            Y1: {
+              type: ArgumentType.STRING
+            },
+            X2: {
+              type: ArgumentType.STRING
+            },
+            Y2: {
+              type: ArgumentType.STRING
+            }
+          },
+          text: 'distance from x: [X1] y: [Y1] to x: [X2] y: [Y2]'
+        }
+      ]
+    }
+  }
+
+Distance (args, util){
+  const X1 = args.X1;
+  const Y1 = args.Y1;
+  const X2 = args.X2;
+  const Y2 = args.Y2;
+
+  return this.write(`bruh`);
+}
+
+}
+
+module.exports = testExt;
